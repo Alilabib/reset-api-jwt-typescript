9f7fd8b9d5343e800884d34adafc20aea646de1d
alilabib <alilabib65@gmail.com>
2022-10-22 23:12:59 +0200

    configure express server

--- build/index.js
+++ build/index.js
@@ -1,1 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.config();
+        this.routes();
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.json({ message: 'Hello World' });
+        });
+    }
+    //start app
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on Port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello world');
+import express,{Request, Response} from 'express';
+
+class Server {
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+  config() {
+    this.app.set('port', process.env.PORT || 3000);
+  }
+  routes(){
+    this.app.get('/',(req:Request,res:Response)=>{
+        res.json({message:'Hello World'}); 
+    });
+  }
+  //start app
+  start() {
+    this.app.listen(this.app.get('port'), () => {
+      console.log('Server on Port', this.app.get('port'));
+    });
+  }
+}
+
+const server = new Server();
+server.start();
